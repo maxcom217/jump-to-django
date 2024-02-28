82913d630b08f61448e12a97ad462fa0c55b7d81
박재웅 <maxcom101@gmail.com>
2024-02-28 23:38:11 +0900

    로그인 만들기

--- common/urls.py
+++ common/urls.py
@@ -8,5 +8,6 @@ from django.contrib.auth import views as auth_views
 app_name = 'common'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='common/login.html'), name='login'),
 ]
