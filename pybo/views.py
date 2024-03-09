a5f25014e9f4a2001684f36e9397913e95f4d90e
박재웅 <maxcom101@gmail.com>
2024-03-10 00:45:58 +0900

    질문추가버튼 삭제버튼 에러수정

--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question, Answer
 from django.utils import timezone
-from .forms import QuestionForm, AnswerForm
+from .forms import QuestionForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -34,7 +34,7 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list' : question_list}
     return render(request, 'pybo/question_list.html', context)
-    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
+    return HttpResponse("oqrhvk.")
 
 
 
@@ -67,13 +67,11 @@ def question_create(request):
     
     pybo 질문등록
     """
-    if form.is_valid():
-            question = form.save(commit=False)
-            question.author = request.user
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
